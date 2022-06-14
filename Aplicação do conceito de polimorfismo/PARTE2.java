cd7bed5bd7d211c492e7a6da10625e4a870dc1bf
João Gabriel Moura <105926520+mourajg@users.noreply.github.com>
2022-06-14 14:49:55 -0300

    Update PARTE2.java

--- Aplicação do conceito de polimorfismo/PARTE2.java
+++ Aplicação do conceito de polimorfismo/PARTE2.java
@@ -1,4 +1,14 @@
 >>>
 package Partes;
 public class mamifero extends animal {
+	private String corPelo;
 	
+	@Override
+	public void locomover() {
+		System.out.println("Correndo");
+	}
+	@Override
+	public void alimentar() {
+		System.out.println("Mamando");
+	}
+}

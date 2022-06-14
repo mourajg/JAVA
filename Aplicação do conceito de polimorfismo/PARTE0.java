05de86b37de3cb41f22d6a2550b48ab3c30bd3a8
João Gabriel Moura <105926520+mourajg@users.noreply.github.com>
2022-06-14 17:35:21 -0300

    Update PARTE0.java

--- Aplicação do conceito de polimorfismo/PARTE0.java
+++ Aplicação do conceito de polimorfismo/PARTE0.java
@@ -5,12 +5,12 @@ public class Partes {
 		manchester_city c = new manchester_city();
 		manchester_united u = new manchester_united();
 		
-		c.gols_time1(3);
-		c.erling_haaland(3);
+		c.setGols_time1(3);
+		c.setErling_haaland(3);
 		c.jogar_em_casa("2 gols do Haaland");
 		c.jogar_fora_de_casa("1 gol do Haaland");
-		u.gols_time2(2);
-		u.Cristiano_Ronaldo(2);
+		u.setGols_time2(2);
+		u.setCristiano_ronaldo(2);
 		u.jogar_em_casa("1 gol do CR7");
 		u.jogar_fora_de_casa("1 gol do CR7");
 	}

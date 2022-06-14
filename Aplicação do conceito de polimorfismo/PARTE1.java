4655ed445270b3d0648bd2f50bb3ef66dc17106d
João Gabriel Moura <105926520+mourajg@users.noreply.github.com>
2022-06-14 16:50:18 -0300

    Update PARTE1.java

--- Aplicação do conceito de polimorfismo/PARTE1.java
+++ Aplicação do conceito de polimorfismo/PARTE1.java
@@ -1,6 +1,6 @@
 >>>
 package Partes;
-public abstract class oitavas {
+public abstract class Oitavas {
 	protected int gols_time1;
 	protected int gols_time2;
 	public abstract void jogar_em_casa();
@@ -8,8 +8,14 @@ public abstract class oitavas {
 	public int getGols_time1() {
 		return gols_time1;
 	}
+	public void setGols_time1(int gols_time1) {
+		this.gols_time1 = gols_time1;
+	}
 	public int getGols_time2() {
 		return gols_time2;
+	}
+	public void setGols_time2(int gols_time2) {
+		this.gols_time2 = gols_time2;
 	}
 }
 >>>

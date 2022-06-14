41d9ffee252939c6dec3b3e9a5c7a78e3182881d
João Gabriel Moura <105926520+mourajg@users.noreply.github.com>
2022-06-14 16:58:03 -0300

    Update PARTE2.java

--- Aplicação do conceito de polimorfismo/PARTE2.java
+++ Aplicação do conceito de polimorfismo/PARTE2.java
@@ -1,7 +1,7 @@
 >>>
 package Partes;
-public class manchester_city extends oitavas {
-	private String Erling_Haaland;
+public class manchester_city extends Oitavas {
+	private String erling_haaland;
 	@Override
 	public void jogar_em_casa() {
 		System.out.println("Etihad Stadium");
@@ -10,11 +10,11 @@ public class manchester_city extends oitavas {
 	public void jogar_fora_de_casa() {
 		System.out.println("Old Trafford");
 	}
-	public String getErling_Haaland() {
-		return Erling_Haaland;
+	public String getErling_haaland() {
+		return erling_haaland;
 	}
-	public void setErling_Haaland(String Erling_Haaland) {
-		this.Erling_Haaland = Erling_Haaland;
+	public void setErling_haaland(String erling_haaland) {
+		this.erling_haaland = erling_haaland;
 	}
 }
 >>>

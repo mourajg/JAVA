08849d869c8174e0eca1135c7787829dd92a9099
João Gabriel Moura <105926520+mourajg@users.noreply.github.com>
2022-06-14 15:50:44 -0300

    Create PARTE3.java

---
+++ Aplicação do conceito de polimorfismo/PARTE3.java
@@ -0,0 +1,15 @@
+>>>
+package Partes;
+public class manchester_united extends oitavas {
+	private String Cristiano_Ronaldo;
+	
+	@Override
+	public void jogar_em_casa() {
+		System.out.println("Old Trafford");
+	}
+	@Override
+	public void jogar_fora_de_casa() {
+		System.out.println("Etihad Stadium");
+	}
+}
+>>>

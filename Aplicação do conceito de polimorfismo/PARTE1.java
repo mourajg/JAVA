33a91cb59f75b62150efe9421c457ad21c52c527
João Gabriel Moura <105926520+mourajg@users.noreply.github.com>
2022-06-14 16:27:30 -0300

    Update PARTE1.java

--- Aplicação do conceito de polimorfismo/PARTE1.java
+++ Aplicação do conceito de polimorfismo/PARTE1.java
@@ -1,10 +1,8 @@
 >>>
 package Partes;
 public abstract class oitavas {
-	/* Atributos de oitavas. */
 	protected int gols_time1;
 	protected int gols_time2;
-	/* Métodos de oitavas. */
 	public abstract void jogar_em_casa();
 	public abstract void jogar_fora_de_casa();
 	public int getGols_time1() {

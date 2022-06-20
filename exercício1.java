339a11e1c46400e4f5dfa1134c78a09d560024a4
João Gabriel Moura <105926520+mourajg@users.noreply.github.com>
2022-06-20 14:16:32 -0300

    Update exercício1.java

--- exercício1.java
+++ exercício1.java
@@ -4,12 +4,10 @@ package exercises;
 public class Medida_de_comprimento {
 	public static void main(String[] args) {
 		double m, dam, hm, km;
-		
 		m = 2; /* Para fazer os cálculos, vamos considerar 2 metros (Poderia ser qualquer valor). */
 		dam = m / 10;
 		hm = m / 100;
 		km = m / 1000;
-		
 		System.out.println("O valor em decâmetros é: " + dam);
 		System.out.println("O valor em hectômetros é: " + hm);
 		System.out.println("O valor em quilômetros é: " + km);

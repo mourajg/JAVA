a5ca064f793232a22806f50b85591b1fceb190d7
João Gabriel Moura <contatomoura2000@gmail.com>
2022-06-22 15:48:07 -0300

    Update exercício3.java

--- exercício3.java
+++ exercício3.java
@@ -1,6 +1,5 @@
 >>>
-/* Desenvolva um programa que, peça ao usuário para inserir uma informação de LOGIN e armazene o que foi inserido em uma variável. Em seguida peça uma SENHA, essa será
-armazenada em outra variável. Por fim, verifique se LOGIN e SENHA são iguais aos que foram definidos. */
+// Desenvolva um programa que verifique se LOGIN e SENHA são iguais aos que foram definidos.
 import java.util.Scanner;
 public class exercises {
 	public static void main(String[] args) {
@@ -12,7 +11,7 @@ public class exercises {
 		if (login.equals("Maria") && senha.equals("123456")) {
             		System.out.printf("Usuário logado com sucesso!");
         	} else {
-            		System.out.println("Inválido!");
+            		System.out.println("Não encontrado!");
 		}
 	}
 }

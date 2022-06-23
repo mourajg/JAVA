bce786a2e3fdf6ba38d9dc5d49377bd8de45a10a
João Gabriel Moura <contatomoura2000@gmail.com>
2022-06-23 00:19:12 -0300

    Update exercício2.java

--- exercício2.java
+++ exercício2.java
@@ -1,16 +1,16 @@
 >>>
-// Desenvolva um programa que, peça ao usuário os salários de um grupo de programadores, calcule o valor médio deles e o exiba.
+// Desenvolva um programa que peça ao usuário os salários de um grupo de programadores, calcule o valor médio deles e o exiba.
 package exercises;
-public class Salários { // Atenção aqui! O programa não irá funcionar com esses acentos.
+public class JG2 {
 	public static void main(String[] args) {
-		double p1, p2, p3, p4, p5, média;
+		double p1, p2, p3, p4, p5, m;
 		p1 = 3500;
 		p2 = 3500;
 		p3 = 3500;
 		p4 = 5500;
 		p5 = 5500;
-		média = (p1 + p2 + p3 + p4 + p5) / 5;
-		System.out.println("O valor médio dos salários deste grupo de programadores é: " + média);
+		m = (p1 + p2 + p3 + p4 + p5) / 5;
+		System.out.println("O valor médio dos salários deste grupo de programadores é: " + m);
 	}
 }
 >>>

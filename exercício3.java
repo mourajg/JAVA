d96787356a5cab891184bb1616ac7bbcf1854aa4
João Gabriel Moura <contatomoura2000@gmail.com>
2022-06-23 21:08:27 -0300

    Update exercício3.java

--- exercício3.java
+++ exercício3.java
@@ -2,19 +2,19 @@
 // Desenvolva um programa que some todos os números em um intervalo determinado pelo usuário.
 import java.util.Scanner;
 public class JG3 {
-	public static void main(String[] args) {
-		Scanner in = new Scanner(System.in);
-		System.out.println("Informe o início do intevalo: ");
-		int ini = in.nextInt();
-		System.out.println("Informe o final do intervalo: ");
-		int fim = in.nextInt();
-		int i = ini;
-		int soma = ini;
-		while(i < fim) {
-			i++;
+	public static void main (String[] args) {
+		Scanner in = new Scanner (System.in);
+		System.out.println ("Abertura do intevalo: ");
+		int abertura = in.nextInt ();
+		System.out.println ("Fim do intervalo: ");
+		int fim = in.nextInt ();
+		int i = abertura;
+		int soma = abertura;
+		while (i < fim) {
+			i ++;
 			soma += i;
 		}
-		System.out.println(soma);
+		System.out.println (soma);
 	}
 }
 >>>

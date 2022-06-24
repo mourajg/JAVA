69a92a335ec55d14121a0009a6e28ec3e15b0c5b
João Gabriel Moura <contatomoura2000@gmail.com>
2022-06-23 21:25:09 -0300

    Update exercício2.java

--- exercício2.java
+++ exercício2.java
@@ -1,6 +1,5 @@
 >>>
 // Desenvolva um programa que peça ao usuário os salários de um grupo de programadores, calcule o valor médio deles e o exiba.
-package exercises;
 public class JG2 {
 	public static void main (String[] args) {
 		double p1, p2, p3, p4, p5, m;

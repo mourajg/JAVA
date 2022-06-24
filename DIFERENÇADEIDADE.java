e7dcc563c1ddfaa7e811ca3378583eb5a2c93eb5
João Gabriel Moura <contatomoura2000@gmail.com>
2022-06-23 22:52:35 -0300

    Update and rename exercício3.java to DIFERENÇADEIDADE.java

---
+++ DIFERENÇADEIDADE.java
@@ -0,0 +1,14 @@
+>>>
+import java.util.Scanner;
+public class DIFERENÇA {
+	public static void main (String [] args) {
+		Scanner in = new Scanner (System.in);
+		System.out.println ("Digite sua idade: ");
+		int idadeEu = in.nextInt ();
+		System.out.println ("Idade de sua namorada: ");
+		int idadeNamorada = in.nextInt ();
+		int d = idadeEu - idadeNamorada;
+		System.out.println ("Sou %d anos mais velho que minha namorada.", d);
+	}
+}
+>>>

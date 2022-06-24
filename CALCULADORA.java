fc1cad651fa266c772c1e0136dca1373fb115175
João Gabriel Moura <contatomoura2000@gmail.com>
2022-06-23 21:37:49 -0300

    Update and rename Calculadora.java to CALCULADORA.java

---
+++ CALCULADORA.java
@@ -0,0 +1,17 @@
+>>>
+public class CALCULADORA {
+	public static void main (String[] args) {
+		double valor1, valor2, a, s, m, d;
+		valor1 = 10; // Insira aqui o primeiro valor.
+		valor2 = 20; // Insira aqui o segundo valor.
+		a = valor1 + valor2;
+		s = valor1 - valor2;
+		m = valor1 * valor2;
+		d = valor1 / valor2;
+		System.out.println ("valor 1 + valor 2 = " + a);
+		System.out.println ("valor 1 - valor 2 = " + s);
+		System.out.println ("valor 1 * valor 2 = " + m);
+		System.out.println ("valor 1 / valor 2 = " + d);
+	}
+}
+>>>

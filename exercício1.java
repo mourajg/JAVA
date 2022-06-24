ee8f5e5ca894cd13c9c12362ea7853b7a2bfe0d7
João Gabriel Moura <contatomoura2000@gmail.com>
2022-06-23 21:25:40 -0300

    Update exercício1.java

--- exercício1.java
+++ exercício1.java
@@ -1,6 +1,5 @@
 >>>
 // Desenvolva um programa que peça ao usuário um valor em segundos, converta e exiba em minutos, horas e dias.
-package exercises;
 public class JG1 {
 	public static void main (String[] args) {
 		double s, m, h, d;

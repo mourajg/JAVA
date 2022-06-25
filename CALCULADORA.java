de5f2a7449a90c7f8fe1e98222dc1b9e87eb5458
João Gabriel Moura <contatomoura2000@gmail.com>
2022-06-25 15:37:10 -0300

    Update CALCULADORA.java

--- CALCULADORA.java
+++ CALCULADORA.java
@@ -4,10 +4,10 @@ public class CALCULADORA {
 		double valor1, valor2, a, s, m, d;
 		valor1 = 10; // Insira aqui o primeiro valor.
 		valor2 = 20; // Insira aqui o segundo valor.
-		a = valor1 + valor2;
-		s = valor1 - valor2;
-		m = valor1 * valor2;
-		d = valor1 / valor2;
+		a = valor1 + valor2; // Adição.
+		s = valor1 - valor2; // Subtração.
+		m = valor1 * valor2; // Multiplicação.
+		d = valor1 / valor2; // Divisão.
 		System.out.println ("valor 1 + valor 2 = " + a);
 		System.out.println ("valor 1 - valor 2 = " + s);
 		System.out.println ("valor 1 * valor 2 = " + m);

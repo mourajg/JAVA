16b341dfaa5b11265d21a2c71e01fe52b4a437bd
João Gabriel Moura <contatomoura2000@gmail.com>
2022-06-30 17:42:12 -0300

    Create TABUADAUSANDOFOR.java

---
+++ TABUADAUSANDOFOR.java
@@ -0,0 +1,16 @@
+>>>
+// Por João Gabriel Amaral de Moura.
+import java.util.Scanner;
+public class TABUADAUSANDOFOR {
+	public static void main (String [] args) {
+		Scanner in = new Scanner (System.in);
+		int x;
+		System.out.printf ("Tabuada de qual número você gostaria de ver? ");
+		int n = in.nextInt ();
+		for (int i = 0; i < 10; i ++) {
+			x = n * (1 + i);
+			System.out.println (x);
+		}
+	}
+}
+>>>

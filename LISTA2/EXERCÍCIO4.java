be2259aaf702a96d772c9119eb97c8f79391b145
João Gabriel Moura <contatomoura2000@gmail.com>
2022-07-01 12:38:28 -0300

    Create EXERCÍCIO4.java

---
+++ LISTA2/EXERCÍCIO4.java
@@ -0,0 +1,20 @@
+>>>
+// Por João Gabriel Amaral de Moura.
+import java.util.Scanner;
+public class EXERCÍCIO4 {
+	public static void main (String [] args) {
+		Scanner in = new Scanner (System.in);
+		double d;
+		System.out.println ("Insira o primeiro valor: ");
+		double valor1 = in.nextDouble ();
+		System.out.println ("Insira o segundo valor: ");
+		double valor2 = in.nextDouble ();
+		if (valor1 >= valor2) {
+			d = valor1 - valor2;
+		} else {
+			d = valor2 - valor1;
+		}
+		System.out.println ("Diferença = " + d);
+	}
+}
+>>>

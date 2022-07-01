74d197a91577875e2d6121b490756c91e108a9ec
João Gabriel Moura <contatomoura2000@gmail.com>
2022-07-01 09:22:00 -0300

    Create EXERCÍCIO10.java

---
+++ LISTA1/EXERCÍCIO10.java
@@ -0,0 +1,22 @@
+// Por João Gabriel Amaral de Moura.
+import java.util.Scanner;
+public class EXERCÍCIO10 {
+        public static void main (String [] args) {
+            Scanner in = new Scanner (System.in);
+		System.out.println ("Valor 1: ");
+		double valor1 = in.nextDouble ();
+		System.out.println ("Valor 2: ");
+		double valor2 = in.nextDouble ();
+		if (valor1 == valor2) {
+			System.out.println ("São iguais!");
+		} else {
+			System.out.println ("São diferentes!");
+		}
+		if (valor1 > valor2) {
+			System.out.println ("O valor 1 é maior que o valor 2.");
+		} else {
+			System.out.println ("O valor 2 é maior que o valor 1.");
+		}
+	}
+}
+>>>

a6b1477cef8438ec1b2eaebe210cfe36e083845f
João Gabriel Moura <contatomoura2000@gmail.com>
2022-07-01 10:14:09 -0300

    Create EXERCÍCIO6.java

---
+++ LISTA2/EXERCÍCIO6.java
@@ -0,0 +1,24 @@
+>>>
+// Por João Gabriel Amaral de Moura.
+import java.util.Scanner;
+public class EXERCÍCIO6 {
+	public static void main (String [] args) {
+		Scanner in = new Scanner (System.in);
+		System.out.println ("Valor 1: ");
+		int valor1 = in.nextInt ();
+		System.out.println ("Valor 2: ");
+		int valor2 = in.nextInt ();
+		if (valor1 == valor2) {
+			System.out.println ("Os valores são iguais!");
+		} else {
+		}
+		if (valor1 > valor2) {
+			System.out.printf ("O valor %s é maior.", valor1);
+			System.out.printf ("O valor %s é menor.", valor2);
+		} else {
+			System.out.printf ("O valor %s é maior.", valor2);
+			System.out.printf ("O valor %s é menor.", valor1);
+		}
+	}
+}
+>>>

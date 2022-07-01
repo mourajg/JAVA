dcdd4a303a4e158b54bbd0a6001314991ca99eea
João Gabriel Moura <contatomoura2000@gmail.com>
2022-06-30 21:22:48 -0300

    Create EXERCÍCIO1.java

---
+++ LISTA1/EXERCÍCIO1.java
@@ -0,0 +1,16 @@
+>>>
+// Por João Gabriel Amaral de Moura.
+import java.util.Scanner;
+public class EXERCÍCIO1 {
+	public static void main (String [] args) {
+		Scanner in = new Scanner (System.in);
+		double m;
+		System.out.println ("Insira o primeiro valor: ");
+		double valor1 = in.nextDouble ();
+		System.out.println ("Insira o segundo valor: ");
+		double valor2 = in.nextDouble ();
+		m = (valor1 + valor2) / 2;
+		System.out.println (m);
+	}
+}
+>>>

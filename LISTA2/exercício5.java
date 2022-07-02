9601b5b65fad347e9f481173f656cd4789b04ff8
João Gabriel Moura <contatomoura2000@gmail.com>
2022-07-02 12:44:36 -0300

    Update exercício5.java

--- LISTA2/exercício5.java
+++ LISTA2/exercício5.java
@@ -1,4 +1,20 @@
 >>>
 // Por João Gabriel Amaral de Moura.
 import java.util.Scanner;
-public class
+public class exercício5 {
+	public static void main (String [] args) {
+		Scanner in = new Scanner (System.in);
+		do {
+			System.out.println ("Insira um valor: ");
+			int n = in.nextInt ();
+			if (n > 0) {
+				System.out.println ("O valor é positivo!");
+			} else {
+			}
+			if (n < 0) {
+				System.out.println ("O valor é negativo!");
+			}
+		} while (n != 0);
+	}
+}
+>>>

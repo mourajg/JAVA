cc9b09450abae5bd14c3d3860274985d589caab0
João Gabriel Moura <contatomoura2000@gmail.com>
2022-07-02 14:50:39 -0300

    Update exercício1.java

--- LISTA3/exercício1.java
+++ LISTA3/exercício1.java
@@ -3,23 +3,44 @@
 import java.util.Scanner;
 public class exercício1 {
 	public static void menu () {
-		System.out.println ("MENU");
-		System.out.println ("1 - Verificar se um dos números é múltiplo do outro.");
-		System.out.println ("2 - Verificar se os dois números são pares.");
-		System.out.println ("3 - Sair.);
-	}
-	public static void verifica1 {
 		Scanner in = new Scanner (System.in);
+		double valor1, valor2, resultado;
+		int menu;
+		boolean loopmenu = true;
 		System.out.println ("Insira o primeiro valor: ");
-		int valor1 = in.nextInt ();
-		System.out.println ("Insira o segundo valor: ");
-		int valor2 = in.nextInt ();
-		if (valor1 % valor2 == 0 || valor2 % valor1 == 0) {
-			System.out.println ("Sim, um dos valores é múltiplo do outro.");
-		} else {
-			System.out.println ("Não são múltiplos.");
+		valor1 = in.nextDouble ();
+		System.out.print("Insira o segundo valor: ");
+		valor2 = in.nextDouble ();
+		while (loopmenu) {
+		System.out.print("1 - Um dos números é ou não múltiplo do outro.\n2 - Os números são pares.\n3 - A média dos números é maior ou igual 7.\n4 - Sair.");
+			menu = in.nextInt ();
+			switch (menu) {
+				case 1:
+					if (valor1 % valor2 ==0 || valor1 % valor2 ==0) {
+						System.out.println ("Um dos números é múltiplo do outro.");
+					} else {
+						System.out.println ("Um não é múltiplo do outro.");
+					}
+					break;
+				case 2:
+					if (valor1 % 2 == 0 && valor2 % 2 == 0) {
+						System.out.println ("São pares!");
+					} else {
+					}
+					break;
+				case 3:
+					if ((valor1 + valor2) / 2 >= 7) {
+						System.out.println ("Positivo!");
+					} else {
+						System.out.println ("Negativo!");
+					}
+					break;
+				case 4:
+					loopmenu = false;
+					break;
+				default:
+					System.out.println ("Opção inválida!");
+			}
 		}
 	}
-	public static void main (String [] args) {
-		Scanner in = new Scanner (System.in);
-		
+}

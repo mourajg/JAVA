4e28aa3496ffb534201dfaec79a66655ac8ff1e0
João Gabriel Moura <contatomoura2000@gmail.com>
2022-07-02 11:53:13 -0300

    Update and rename EXERCÍCIO9.java to exercício4.java

--- LISTA1/exercício4.java
+++ LISTA1/exercício4.java
@@ -1,17 +1,18 @@
+>>>
 // Por João Gabriel Amaral de Moura.
 import java.util.Scanner;
-public class EXERCÍCIO9 {
-        public static void main (String [] args) {
-            Scanner in = new Scanner (System.in);
-		int d;
+public class exercício4 {
+	public static void main (String [] args) {
+		Scanner in = new Scanner (System.in);
+		int idadeemdias;
 		System.out.println ("IDADE - anos: ");
 		int anos = in.nextInt ();
 		System.out.println ("IDADE - meses: ");
 		int meses = in.nextInt ();
 		System.out.println ("IDADE - dias: ");
 		int dias = in.nextInt ();
-		d = dias + (meses * 30) + (anos * 365);
-		System.out.println ("Idade em dias: " + d);
+		idadeemdias = dias + (meses * 30) + (anos * 365);
+		System.out.println ("Idade em dias: " + idadeemdias);
 	}
 }
 >>>

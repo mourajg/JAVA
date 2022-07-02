3c038d5ad92c472c0b297637127ed33cb8303691
João Gabriel Moura <contatomoura2000@gmail.com>
2022-07-02 12:22:38 -0300

    Update and rename EXERCÍCIO6.java to exercício2.java

--- LISTA2/exercício2.java
+++ LISTA2/exercício2.java
@@ -1,7 +1,7 @@
 >>>
 // Por João Gabriel Amaral de Moura.
 import java.util.Scanner;
-public class EXERCÍCIO6 {
+public class exercício2 {
 	public static void main (String [] args) {
 		Scanner in = new Scanner (System.in);
 		System.out.println ("Valor 1: ");
@@ -14,10 +14,10 @@ public class EXERCÍCIO6 {
 		}
 		if (valor1 > valor2) {
 			System.out.printf ("O valor %s é maior.", valor1);
-			System.out.printf ("O valor %s é menor.", valor2);
+			System.out.printf ("\nO valor %s é menor.", valor2);
 		} else {
 			System.out.printf ("O valor %s é maior.", valor2);
-			System.out.printf ("O valor %s é menor.", valor1);
+			System.out.printf ("\nO valor %s é menor.", valor1);
 		}
 	}
 }

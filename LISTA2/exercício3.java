f2f4d2db49bc943c734fe33037ba5136017f082a
João Gabriel Moura <contatomoura2000@gmail.com>
2022-07-02 12:25:35 -0300

    Update exercício3.java

--- LISTA2/exercício3.java
+++ LISTA2/exercício3.java
@@ -1,7 +1,7 @@
 >>>
 // Por João Gabriel Amaral de Moura.
 import java.util.Scanner;
-public class EXERCÍCIO8 {
+public class exercício3 {
 	public static void main (String [] args) {
 		Scanner in = new Scanner (System.in);
 		System.out.println ("Insira um valor: ");

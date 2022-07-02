748d802502ad4d50c09046ff01937bfdaed30cdf
João Gabriel Moura <contatomoura2000@gmail.com>
2022-07-02 15:19:19 -0300

    Create exercício2.java

---
+++ LISTA3/exercício2.java
@@ -0,0 +1,15 @@
+>>>
+// Por João Gabriel Amaral de Moura.
+public class exercício2 {
+	public static void main (String [] args) {
+		int n;
+		for (n = 1; n <= 100; n++) {
+			if (n % 10 == 0) {
+				System.out.println (n + " múltiplo de 10.");
+			} else {
+				System.out.println (n);
+			}
+		}
+	}
+}
+>>>

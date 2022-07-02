da7e11673721c2ab84d5f07f9f8630571469d895
João Gabriel Moura <contatomoura2000@gmail.com>
2022-07-02 11:58:06 -0300

    Update and rename EXERCÍCIO10.java to exercício5.java

--- LISTA1/exercício5.java
+++ LISTA1/exercício5.java
@@ -1,16 +1,17 @@
+>>>
 // Por João Gabriel Amaral de Moura.
 import java.util.Scanner;
-public class EXERCÍCIO10 {
-        public static void main (String [] args) {
-            Scanner in = new Scanner (System.in);
+public class exercício5 {
+	public static void main (String [] args) {
+		Scanner in = new Scanner (System.in);
 		System.out.println ("Valor 1: ");
 		double valor1 = in.nextDouble ();
 		System.out.println ("Valor 2: ");
 		double valor2 = in.nextDouble ();
 		if (valor1 == valor2) {
-			System.out.println ("São iguais!");
+			System.out.println ("Valores iguais!");
 		} else {
-			System.out.println ("São diferentes!");
+			System.out.println ("Valores diferentes!");
 		}
 		if (valor1 > valor2) {
 			System.out.println ("O valor 1 é maior que o valor 2.");

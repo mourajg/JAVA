af439d72d4ea86f90662f4dd8a1f2c3289a1e550
João Gabriel Moura <contatomoura2000@gmail.com>
2022-07-04 08:13:57 -0300

    Update exercício5.java

--- LISTA3/exercício5.java
+++ LISTA3/exercício5.java
@@ -1,20 +1,18 @@
+>>>
+// Por João Gabriel Amaral de Moura.
 import java.util.Scanner;
-
-public class Exercicio {
-
-	public static void main(String[] args) {
-		Scanner entrada = new Scanner(System.in);
-		System.out.print("Digite o salario atual: ");
-		double SalarioAtual = entrada.nextDouble();
-		System.out.print("Digite o percentual de reajuste: ");
-		double Reajuste = (entrada.nextDouble()/100);
-		System.out.println("O novo salario é de R$"+String.format( "%.2f", Reajuste(SalarioAtual,Reajuste) )+".");
+public class exercício5 {
+	public static void main (String [] args) {
+		Scanner in = new Scanner (System.in);
+		System.out.println ("Insira o salário atual: ");
+		double salárioatual = in.nextDouble ();
+		System.out.println ("Insira o percentual de reajuste: ");
+		double reajuste = (in.nextDouble () / 100);
+		System.out.println ("O novo salário é: " + String.format ("%.2f", reajuste (salárioatual, reajuste)) + ".");
 	}
-	public static double Reajuste(double Salario,double IndiceReajuste){
-		double NovoSalario = Salario + (Salario*IndiceReajuste);
-		return NovoSalario;
+	public static double reajuste (double salário, double índicereajuste) {
+		double novosalário = salário + (salário * índicereajuste);
+		return novosalário;
 	}
-
-
-
 }
+>>>

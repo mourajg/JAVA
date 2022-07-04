53c7db46afa082fc52b8e477d2fea4dc0c14e925
João Gabriel Moura <contatomoura2000@gmail.com>
2022-07-04 08:14:52 -0300

    Update exercício2.java

--- LISTA3/exercício2.java
+++ LISTA3/exercício2.java
@@ -3,7 +3,7 @@
 public class exercício2 {
 	public static void main (String [] args) {
 		int n;
-		for (n = 1; n <= 100; n++) {
+		for (n = 1; n <= 100; n ++) {
 			if (n % 10 == 0) {
 				System.out.println (n + " múltiplo de 10.");
 			} else {

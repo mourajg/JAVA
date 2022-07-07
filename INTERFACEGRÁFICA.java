192210ac1542e150ec063770e7caa17bd3cd209a
João Gabriel Moura <contatomoura2000@gmail.com>
2022-07-07 19:23:13 -0300

    Create INTERFACEGRÁFICA.java

---
+++ INTERFACEGRÁFICA.java
@@ -0,0 +1,14 @@
+>>>
+// Por João Gabriel Amaral de Moura.
+package PROJETOCALCULADORA;
+import javax.swing.WindowConstants;
+import javax.swing.JFrame;
+public class TELA extends JFrame {
+	public static void main (String [] args) {
+		JFrame janela = new JFrame ("CALCULADORA");
+		janela.setBounds (200, 500, 400, 400);
+		janela.setDefaultCloseOperation (WindowConstants.DISPOSE_ON_CLOSE);
+		janela.setVisible (true);
+	}
+}
+>>>

5454145df130bc744d6432b228e1d7af1ac3e360
João Gabriel Moura <contatomoura2000@gmail.com>
2022-07-07 19:33:43 -0300

    Update INTERFACEGRÁFICA.java

--- INTERFACEGRÁFICA.java
+++ INTERFACEGRÁFICA.java
@@ -1,13 +1,13 @@
 >>>
 // Por João Gabriel Amaral de Moura.
 package PROJETOCALCULADORA;
-import javax.swing.WindowConstants;
-import javax.swing.JFrame;
+import javax.swing.WindowConstants; // Usa.
+import javax.swing.JFrame; // Usa.
 public class TELA extends JFrame {
 	public static void main (String [] args) {
-		JFrame janela = new JFrame ("CALCULADORA");
-		janela.setBounds (200, 500, 400, 400);
-		janela.setDefaultCloseOperation (WindowConstants.DISPOSE_ON_CLOSE);
+		JFrame janela = new JFrame ("CALCULADORA"); // Título da janela.
+		janela.setBounds (200, 500, 400, 400); // Definir tamanho da janela.
+		janela.setDefaultCloseOperation (WindowConstants.DISPOSE_ON_CLOSE); // Permite fechar.
 		janela.setVisible (true);
 	}
 }

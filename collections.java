41c833d6b6bbebc6a6e93bd0eb5279c19eba7049
João Gabriel Moura <contatomoura2000@gmail.com>
2022-07-22 08:31:38 -0300

    Update and rename COLLECTIONS.java to collections.java

--- collections.java
+++ collections.java
@@ -3,9 +3,9 @@
 import java.util.ArrayList;
 import java.util.Collections; // Sempre usada quando tratamos de Collections.
 import java.util.List;
-public class COLLECTIONS {
+public class collections {
 	public static void main (String [] args) {
-		List <String> nomes = new ArrayList <> (); // Lista do tipo String que se chama nomes.
+		List <String> nomes = new ArrayList <> ();
 		nomes.add ("Flamengo");
 		nomes.add ("Fluminense");
 		nomes.add ("Palmeiras");
@@ -21,25 +21,6 @@ public class COLLECTIONS {
 		boolean temSantos = nomes.contains ("Santos"); // Retorna true se o nome estiver na lista.
 		nomes.clear (); // Limpa tudo.
 		int posição = nomes.indexOf ("Palmeiras"); // Retorna o posicionamento do time na lista.
-	}
-}
->>>
-import java.util.LinkedList;
-import java.util.Queue; // Fila.
-public class COLLECTIONS {
-	public static void main (String [] args) {
-		Queue <String> filaBanco = new LinkedList <> ();
-		filaBanco.add ("Maria");
-		filaBanco.add ("João");
-		filaBanco.add ("Aparecida");
-		filaBanco.add ("Marcela");
-		filaBanco.add ("Flávia");
-		System.out.println (filaBanco);
-		String clienteAtendido = filaBanco.poll (); // Mostra quem é o primeiro que será atendido, removendo este do restante da fila.
-		System.out.println (clienteAtendido);
-		System.out.println (filaBanco); // O restante da fila.
-		String primeiroCliente = filaBanco.peek (); // Mostra quem é o primeiro da fila sem remover.
-		System.out.println (primeiroCliente);
 	}
 }
 >>>

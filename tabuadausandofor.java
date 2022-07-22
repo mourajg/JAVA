7eaf7b918e6c1521ba27328abca91944594d7c60
João Gabriel Moura <contatomoura2000@gmail.com>
2022-07-22 00:12:32 -0300

    Update tabuadausandofor.java

--- tabuadausandofor.java
+++ tabuadausandofor.java
@@ -1,11 +1,11 @@
 >>>
 // Por João Gabriel Amaral de Moura.
 import java.util.Scanner;
-public class TABUADAUSANDOFOR {
+public class tabuadausandofor {
 	public static void main (String [] args) {
 		Scanner in = new Scanner (System.in);
 		int x;
-		System.out.printf ("Tabuada de qual número você gostaria de ver? ");
+		System.out.println ("Tabuada de qual número você gostaria de ver? ");
 		int n = in.nextInt ();
 		for (int i = 0; i < 10; i ++) {
 			x = n * (1 + i);
